2aa3ec336605582b43b7114f056ef863a571806f
Phil Opaola <phillipsopaolacoding@gmail.com>
2013-08-24 07:27:27 -0400

    Add logger_factory to utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import logging
+
 from argparse import ArgumentParser
 from getpass import getpass
 
@@ -9,3 +11,14 @@ def get_auth_info():
 	auth_info = vars(parser.parse_args())
 	auth_info['password'] = password
 	return auth_info
+
+def logger_factory(name, filename='default'):
+	logger = logging.getLogger(name)
+	logger.setLevel(logging.DEBUG)
+	if filename == 'default':
+		filename = name + '.log'
+	file_handler = logging.FileHandler(filename)
+	format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+	file_handler.setFormatter(format)
+	logger.addHandler(file_handler)
+	return logger
