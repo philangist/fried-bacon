0344ad10d55d01b296fdce189305877893a039bd
Phil Opaola <phillipsopaolacoding@gmail.com>
2013-09-14 22:28:39 -0400

    Change whitespace from tabs to spaces

--- fried_bacon.py
+++ fried_bacon.py
@@ -2,37 +2,37 @@ import utils
 import sys
 import json
 from reddit import (
-	RedditAPIClient,
-	RedditUser,
+    RedditAPIClient,
+    RedditUser,
 )
 
 AUTH_ERROR_MESSAGE = """Your account could not be authenticated. Please ensure
 that your provided credentials are correct/ It's also possible that Reddit's API
-is rate limiting. Try again in a few minutes"""
+is rate limitingl. Try again in a few minutes"""
 
 if __name__ == '__main__':
-	auth_info = utils.get_auth_info()
-	username, password = auth_info['username'], auth_info['password']
-	delete_content = auth_info.get('content_delete')
-	delete_self = auth_info.get('self_delete')
-	reddit_user = RedditUser(username, password)
-	print 'Connecting to Reddit...'
-	if reddit_user.login():
-		print 'Accessing comments and self posts'
-		download = reddit_user.download_self()
-		if download[0] == download[1] == 0:
-			print 'No comments or posts found on that account'
-			sys.exit()
-		print 'Found %d submitted posts' % download[0]
-		print 'Found %d submitted comments' % download[1]
-		print 'Editing comments and self posts (This might take a few minutes)'
-		reddit_user.edit_content()
-		if delete_content:
-			print 'Deleting comments and self posts (We\'re almost done!)'
-			reddit_user.delete_content()
-		if delete_self:
-			print 'Deleting account'
-			reddit_user.delete_self()
-		print 'Done!'
-	else:
-		print AUTH_ERROR_MESSAGE
+    auth_info = utils.get_auth_info()
+    username, password = auth_info['username'], auth_info['password']
+    delete_content = auth_info.get('content_delete')
+    delete_self = auth_info.get('self_delete')
+    reddit_user = RedditUser(username, password)
+    print 'Connecting to Reddit...'
+    if reddit_user.login():
+        print 'Accessing comments and self posts'
+        download = reddit_user.download_self()
+        if download[0] == download[1] == 0:
+            print 'No comments or posts found on that account'
+            sys.exit()
+        print 'Found %d submitted posts' % download[0]
+        print 'Found %d submitted comments' % download[1]
+        print 'Editing comments and self posts (This might take a few minutes)'
+        reddit_user.edit_content()
+        if delete_content:
+            print 'Deleting comments and self posts (We\'re almost done!)'
+            reddit_user.delete_content()
+        if delete_self:
+            print 'Deleting account'
+            reddit_user.delete_self()
+        print 'Done!'
+    else:
+        print AUTH_ERROR_MESSAGE
--- utils.py
+++ utils.py
@@ -4,23 +4,23 @@ from argparse import ArgumentParser
 from getpass import getpass
 
 def get_auth_info():
-	parser = ArgumentParser(description='Bulk edit all your Reddit comments'
-										'nonsense and delete your account')
-	parser.add_argument('-u', '--username', required=True)
-	parser.add_argument('-c', '--content-delete', action='store_true', default=False)
-	parser.add_argument('-s', '--self-delete', action='store_true', default=False)
-	auth_info = vars(parser.parse_args())
-	password = getpass()
-	auth_info['password'] = password
-	return auth_info
+    parser = ArgumentParser(description='Bulk edit all your Reddit comments'
+                                        'nonsense and delete your account')
+    parser.add_argument('-u', '--username', required=True)
+    parser.add_argument('-c', '--content-delete', action='store_true', default=False)
+    parser.add_argument('-s', '--self-delete', action='store_true', default=False)
+    auth_info = vars(parser.parse_args())
+    password = getpass()
+    auth_info['password'] = password
+    return auth_info
 
 def logger_factory(name, filename='default'):
-	logger = logging.getLogger(name)
-	logger.setLevel(logging.DEBUG)
-	if filename == 'default':
-		filename = name + '.log'
-	file_handler = logging.FileHandler(filename)
-	format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
-	file_handler.setFormatter(format)
-	logger.addHandler(file_handler)
-	return logger
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    if filename == 'default':
+        filename = name + '.log'
+    file_handler = logging.FileHandler(filename)
+    format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+    file_handler.setFormatter(format)
+    logger.addHandler(file_handler)
+    return logger
